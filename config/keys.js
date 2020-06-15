74c5c6e10d9c3dd756e2e89e410e2ea02456f826
jake-schroeder-isx <jake.schroeder@isophex.com>
2020-06-14 21:11:05 -0400

    removed local reference to api key

--- config/keys.js
+++ config/keys.js
@@ -3,6 +3,5 @@
 // let pass = process.env.PASS;
 
 module.exports = {
-  mongoURI: `mongodb+srv://jake-dev:6U2uEOSxNAvaYhaP@cluster0-e5oro.mongodb.net/presence?retryWrites=true&w=majority`,
   secretOrKey: "secret",
 };
